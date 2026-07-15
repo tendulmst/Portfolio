a2d1f06b3972c578cd149a97ea1e63592500c6c0
Tendulkar <tendulkarm2000@gmail.com>
2026-07-15 17:01:57 +0530

    Fixed accessibility issue

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -59,7 +59,7 @@ const Projects = () => {
                   ))}
                 </div>
                 <div className="project-footer">
-                  <a className="live-btn" href={p.live} target="_blank" rel="noreferrer"></a>
+                  <a className="live-btn" href={p.live} target="_blank" rel="noreferrer">View project</a>
                 </div>
               </div>
             </article>
